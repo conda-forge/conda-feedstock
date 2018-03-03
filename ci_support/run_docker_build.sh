8ab3803cad461429119f1d591917c9b82313e706
Kale Franz <kfranz@continuum.io>
2018-03-03 00:11:42 -0600

    try addin --no-activate

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -58,7 +58,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-conda build /recipe_root --quiet || exit 1
+conda build /recipe_root --no-activate --quiet || exit 1
 upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
 touch /feedstock_root/build_artefacts/conda-forge-build-done
