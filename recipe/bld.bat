397faa7f83fa073f11eb4f95b279245778689f57
Kale Franz <kfranz@continuum.io>
2018-09-07 17:00:49 -0500

    debug
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,9 @@ echo %PKG_VERSION% > conda\.version
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+set
+%PYTHON% -m pip list
+%PYTHON% -m conda info -a
+
 %PYTHON% -m conda init --install
 if %errorlevel% neq 0 exit /b %errorlevel%
