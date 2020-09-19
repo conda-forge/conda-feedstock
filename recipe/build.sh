454fa14d579d0a1c501398f7bed2e25390c8a48a
Isuru Fernando <isuruf@gmail.com>
2020-09-19 00:52:34 -0500

    Need CONDA_PREFIX set for the scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ if [[ "$build_platform" != "$target_platform" && -z "$PYTHONPATH" ]]; then
     # See https://github.com/conda/conda-build/blob/1010e8309a20e144c51b1d86b2daebd146be923e/conda_build/metadata.py#L2273
     # I don't know why 'conda' is special cased there, but let's try activate here and deactivate at the end.
     RUN_DEACTIVATE_MANUALLY=1
+    export CONDA_PREFIX=$BUILD_PREFIX
     for file in `ls $BUILD_PREFIX/etc/conda/activate.d/*.sh | sort -V`; do
         source $file
     done
