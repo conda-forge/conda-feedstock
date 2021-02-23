7f64ca9e0e758570d6de1f4219bd6ef3a9339104
Allan Leal <allanmulin@gmail.com>
2021-02-23 23:53:18 +0100

    Create recipe for sciplot

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,22 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-# Deactivate external conda.
-source deactivate
-
-# Add and configure special conda directories and files.
-export CONDARC="$PREFIX/.condarc"
-export CONDA_ENVS_DIRS="$PREFIX/envs"
-export CONDA_PKGS_DIRS="$PREFIX/pkgs"
-touch "$CONDARC"
-mkdir "$CONDA_ENVS_DIRS"
-mkdir "$CONDA_PKGS_DIRS"
-
-# Activate the built conda.
-source $PREFIX/bin/activate $PREFIX
-
-# Run conda tests.
-source ./test_conda.sh
-
-# Deactivate the built conda when done.
-# Not necessary, but a good test.
-source deactivate
+cd test
+mkdir build
+cd build
+cmake .. -GNinja -DCMAKE_PREFIX_PATH=$PREFIX
+ninja
+./test
