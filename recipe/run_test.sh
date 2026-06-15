c940d3d75b15191f0f305361a41b914d584313c1
jaimergp <jaimergp@users.noreply.github.com>
2026-06-15 11:58:47 +0200

    Convert to recipe.yaml

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -13,7 +13,7 @@ eval "$(python -m conda shell.bash hook)"
 conda info --all
 
 # create, activate, and deactivate a conda environment
-conda create --yes --prefix "./built-conda-test-env" "patch" || exit 1
+conda create --yes -c conda-forge --prefix "./built-conda-test-env" "patch" || exit 1
 
 conda activate "./built-conda-test-env"
 echo "CONDA_PREFIX=${CONDA_PREFIX}"
